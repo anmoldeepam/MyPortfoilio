60d465dde6f3945944c677c394c9480c3ea0b346
AnmolDeep <amanmoldeep@gmail.com>
2023-11-23 01:20:35 +0100

    After deleting Email

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         <HeroSection />
         <AboutMe />
         <ProjectSection/>
-        <EmailSection />
+        
       </div>
       <Footer />
     </main>
